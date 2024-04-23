0240e12f33533c9e2d2b6f13524b10a59c43e589
TimoMen <100569212+timomen@users.noreply.github.com>
2024-04-23 16:57:36 +0200

    new icon now pls work also thx gemini

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,30 +1,32 @@
-const staticPwa = "PWA-APP-SITE"
+const staticPwa = "PWA-APP-SITE";
 const assets = [
-    "/",
-    "/index.html"
-]
+  "/",
+  "/index.html",
+  "/icon.png"
+];
 
 self.addEventListener("install", installEvent => {
-    installEvent.waitUntil(
-        caches.open(staticPwa).then(cache => {
-            cache.addAll(assets)
-        })
-    )
-})
+  installEvent.waitUntil(
+    caches.open(staticPwa).then(cache => {
+      console.log("Caching assets:", assets);  // Add logging to see if caching is successful
+      return cache.addAll(assets);
+    })
+  );
+});
 
 self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then(res => {
-            return res || fetch(fetchEvent.request)
-        })
-    )
-})
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
 
 if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function () {
-        navigator.serviceWorker
-            .register("/serviceWorker.js")
-            .then(res => console.log("service worker registered"))
-            .catch(err => console.log("service worker not registered", err))
-    })
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("Service worker registered"))
+      .catch(err => console.error("Service worker not registered:", err));
+  });
 }
