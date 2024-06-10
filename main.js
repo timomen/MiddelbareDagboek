2b67e578b1b2005d9fa98800938859e8eff34450
Typfout <100569212+timomen@users.noreply.github.com>
2024-06-10 08:11:04 +0200

    calendar page partial functionality tab switching, improved code, unfinished settings page

--- main.js
+++ main.js
@@ -1,34 +1,24 @@
 // handle nav buttons clicked
-document.getElementById("nav-new").addEventListener("click", function() {
-    document.getElementById("nav-new").style.backgroundColor = "#212529";
-    document.getElementById("nav-home").style.backgroundColor = "transparent";
-    document.getElementById("nav-calendar").style.backgroundColor = "transparent";
-    document.getElementById("page-home").style.display = "none";
-    document.getElementById("page-calendar").style.display = "none";
-    document.getElementById("page-new").style.display = "block";
-});
-document.getElementById("nav-home").addEventListener("click", function() {
-    document.getElementById("nav-home").style.backgroundColor = "#212529";
-    document.getElementById("nav-new").style.backgroundColor = "transparent";
-    document.getElementById("nav-calendar").style.backgroundColor = "transparent";
-    document.getElementById("page-home").style.display = "block";
-    document.getElementById("page-calendar").style.display = "none";
-    document.getElementById("page-new").style.display = "none";
-});
 
-document.getElementById("nav-calendar").addEventListener("click", function() {
-    document.getElementById("nav-calendar").style.backgroundColor = "#212529";
-    document.getElementById("nav-new").style.backgroundColor = "transparent";
-    document.getElementById("nav-home").style.backgroundColor = "transparent";
-    document.getElementById("page-home").style.display = "none";
-    document.getElementById("page-calendar").style.display = "block";
-    document.getElementById("page-new").style.display = "none";
-});
+function changeTab(navclicked) {
+    navclicked.style.backgroundColor = "#212529";
+    document.getElementById("page-settings").style.display = "none";
+    document.getElementById("page" + navclicked.id.slice(3)).style.display = "block";
+    for (var i = 0; i < navclicked.parentElement.children.length; i++) {
+        if (navclicked.parentElement.children[i] !== navclicked) {
+            navclicked.parentElement.children[i].style.backgroundColor = "transparent";
+            document.getElementById("page" + navclicked.parentElement.children[i].id.slice(3)).style.display = "none";
+        }
+    }
+}
+
+for (var i = 0; i < document.getElementById("mobile-nav").children.length; i++) {
+    document.getElementById(document.getElementById("mobile-nav").children[i].id).addEventListener("click", function () { changeTab(this); });
+}
 
 // page 'new'
 
-// people
-document.getElementById("page-new-peopleinput").addEventListener("keypress", function(e) {
+document.getElementById("page-new-peopleinput").addEventListener("keypress", function (e) {
     if (e.key == "Enter") {
         var newbadge = document.createElement("span");
         newbadge.className = "badge text-bg-light mx-1";
@@ -37,4 +27,37 @@ document.getElementById("page-new-peopleinput").addEventListener("keypress", fun
         document.getElementById("page-new-people").style.marginLeft = "4%";
         document.getElementById("page-new-peopleinput").value = "";
     }
+});
+
+// page 'home'
+
+document.getElementById("page-home-tuneicon").addEventListener("click", function () {
+    document.getElementById("page-home").style.display = "none";
+    document.getElementById("page-settings").style.display = "block";
+});
+
+// page 'calendar'
+
+function highlightBtn(btntohighlite) {
+    btntohighlite.classList.add("btn-primary")
+    btntohighlite.classList.remove("btn-outline-primary");
+    document.getElementById("page-calendar" + btntohighlite.id.slice(22)).style.display = "block";
+    for (var i = 0; i < btntohighlite.parentElement.children.length; i++) {
+        if (btntohighlite.parentElement.children[i] !== btntohighlite) {
+            btntohighlite.parentElement.children[i].classList.add("btn-outline-primary");
+            btntohighlite.parentElement.children[i].classList.remove("btn-primary");
+            document.getElementById("page-calendar" + btntohighlite.parentElement.children[i].id.slice(22)).style.display = "none";
+        }
+    }
+}
+
+for (var i = 0; i < document.getElementById("page-calendar-btngroup").children.length; i++) {
+    document.getElementById(document.getElementById("page-calendar-btngroup").children[i].id).addEventListener("click", function () { highlightBtn(this); });
+}
+
+// page 'settings'
+
+document.getElementById("page-settings-closeicon").addEventListener("click", function () {
+    document.getElementById("page-settings").style.display = "none";
+    document.getElementById("page-home").style.display = "block";
 });
